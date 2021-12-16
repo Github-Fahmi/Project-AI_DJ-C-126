e8a6424f79eca103d10d2352c3328f28971c6748
Github-Fahmi <95966700+Github-Fahmi@users.noreply.github.com>
2021-12-16 12:42:14 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
-song=""
+song_1="";
+song_2="";
+function preload() {
+    song_1=loadSound("music.mp3");
+    song_2=loadSound("music2.mp3");
+}
+function setup() {
+   video=createCapture(VIDEO)
+    canvas=createCanvas(600, 500);
+    canvas.center();
+    video.hide();
+}
+function draw() {
+    image(video, 0, 0, 600, 500)
+}
